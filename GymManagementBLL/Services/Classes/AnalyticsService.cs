446189bae184bb7513a6603dd0d37b1f7757fd49
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-12-09 22:48:25 +0200

    7- HomePage _AnalyticsService

---
+++ GymManagementBLL/Services/Classes/AnalyticsService.cs
@@ -0,0 +1,35 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.Services.Classes
+{
+    public class AnalyticsService : IAnalyticsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AnalyticsService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public AnalyticsViewModel? GetAnalyticsData()
+        {
+            var sessionRepository = _unitOfWork.GetRepository<Session>();
+            return new AnalyticsViewModel
+            {
+                ActiveMembers = _unitOfWork.GetRepository<Membership>().GetAll(x => x.Status == "Active").Count(),
+                TotalMembers = _unitOfWork.GetRepository<Member>().GetAll().Count(),
+                TotalTrainers = _unitOfWork.GetRepository<Trainer>().GetAll().Count(),
+                UpComingSessions = sessionRepository.GetAll(s => s.StartDate > DateTime.Now).Count(),
+                OngoingSessions =  sessionRepository.GetAll(s => s.StartDate <= DateTime.Now && s.EndDate >= DateTime.Now).Count(),
+                CompletedSessions = sessionRepository.GetAll(s => s.EndDate < DateTime.Now).Count(),
+            };
+        }
+    }
+}
