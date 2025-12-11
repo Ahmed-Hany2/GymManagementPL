5bbafbc772e89dd08a3c3f9a31c768385f12e26b
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-12-11 18:58:18 +0200

    1- PlanController _ Index

---
+++ GymManagementPL/Controllers/PlanController.cs
@@ -0,0 +1,20 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementPL.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly IplanService _planService;
+
+        public PlanController(IplanService planService)
+        {
+            _planService = planService;
+        }
+        public IActionResult Index()
+        {
+            var plans = _planService.GetAllPlans();
+            return View(plans);
+        }
+    }
+}
